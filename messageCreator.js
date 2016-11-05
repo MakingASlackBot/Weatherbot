347fc06d96cbc2f7e6a9b5a8eca8d6c7c9e29ae9
Seagullible <natlockhart@gmail.com>
2016-11-04 23:05:51 -0500

    Parsed Data

--- messageCreator.js
+++ messageCreator.js
@@ -2,15 +2,18 @@ var messageCreator = function () {};
 
 
 messageCreator.prototype.getWeather = function(bot,message,controller,inputLocation){
+	var parsedData = wundergroundGet(inputLocation);
+	parseForecastMessage(bot,message,controller,location,parsedData);
+}
+
+var wundergroundGet = function(inputLocation){
 	var request = require('request');
 	var rawLocation = inputLocation;
 	var location = rawLocation.split(',');
-	var url = 'http://api.wunderground.com/api/e6d58e1b342bc28a/geolookup/conditions/q/' + location[1] + '/' + location[0] + '.json';
-	
+	var url = 'http://api.wunderground.com/api/e6d58e1b342bc28a/forecast/q/' + location[1] + '/' + location[0] + '.json';
 	request(url, function(error, response, data){
 		if (!error && response.statusCode == 200){
 			var parsedData = JSON.parse(data);
-			parseWeatherMessage(bot,message,controller,location,parsedData);
 		}
 	});
 }
@@ -45,17 +48,17 @@ var parseWeatherMessage = function(bot,message,controller,location,parsedData){
 
 var parseForecastMessage = function(bot,message,controller,location,parsedData){
 	if(parsedData.forecast != null){
-				if(location[2] == null){
-					controller.storage.users.get(message.user, function(err, user) {        
-					bot.reply(message, 'Today\'s ' + location[0] + ' forecast: ' + parsedData.forecast.simpleforecast.forecastday[0].conditions + '. High is ' + parsedData.forecast.simpleforecast.forecastday[0].high.fahrenheit + ', low is ' + parsedData.forecast.simpleforecast.forecastday[0].low.fahrenheit + '. ' + parsedData.forecast.simpleforecast.forecastday[0].avewind.mph + ' mph wind, ' + parsedData.forecast.simpleforecast.forecastday[0].avehumidity + '% humidity. Chance of precipitation: ' + parsedData.forecast.simpleforecast.forecastday[0].pop + '%. Plan for ' +  parsedData.forecast.simpleforecast.forecastday[0].snow_allday.in + ' inches of snow.');
-					});			
-				}
-				else{
-					controller.storage.users.get(message.user, function(err, user) {       
-						bot.reply(message, location[0] + ' forecast for ' + location[2] + ' days from now: ' + parsedData.forecast.simpleforecast.forecastday[location[2]].conditions + '. High is ' + parsedData.forecast.simpleforecast.forecastday[location[2]].high.fahrenheit + ', low is ' + parsedData.forecast.simpleforecast.forecastday[location[2]].low.fahrenheit + '. ' + parsedData.forecast.simpleforecast.forecastday[location[2]].avewind.mph + ' mph wind, ' + parsedData.forecast.simpleforecast.forecastday[location[2]].avehumidity + '% humidity. Chance of precipitation: ' + parsedData.forecast.simpleforecast.forecastday[location[2]].pop + '%. Plan for ' +  parsedData.forecast.simpleforecast.forecastday[location[2]].snow_allday.in + ' inches of snow.');
-					});						
-				}
-			}
+		if(location[2] == null){
+			controller.storage.users.get(message.user, function(err, user) {        
+			bot.reply(message, 'Today\'s ' + location[0] + ' forecast: ' + parsedData.forecast.simpleforecast.forecastday[0].conditions + '. High is ' + parsedData.forecast.simpleforecast.forecastday[0].high.fahrenheit + ', low is ' + parsedData.forecast.simpleforecast.forecastday[0].low.fahrenheit + '. ' + parsedData.forecast.simpleforecast.forecastday[0].avewind.mph + ' mph wind, ' + parsedData.forecast.simpleforecast.forecastday[0].avehumidity + '% humidity. Chance of precipitation: ' + parsedData.forecast.simpleforecast.forecastday[0].pop + '%. Plan for ' +  parsedData.forecast.simpleforecast.forecastday[0].snow_allday.in + ' inches of snow.');
+			});			
+		}
+		else{
+			controller.storage.users.get(message.user, function(err, user) {       
+				bot.reply(message, location[0] + ' forecast for ' + location[2] + ' days from now: ' + parsedData.forecast.simpleforecast.forecastday[location[2]].conditions + '. High is ' + parsedData.forecast.simpleforecast.forecastday[location[2]].high.fahrenheit + ', low is ' + parsedData.forecast.simpleforecast.forecastday[location[2]].low.fahrenheit + '. ' + parsedData.forecast.simpleforecast.forecastday[location[2]].avewind.mph + ' mph wind, ' + parsedData.forecast.simpleforecast.forecastday[location[2]].avehumidity + '% humidity. Chance of precipitation: ' + parsedData.forecast.simpleforecast.forecastday[location[2]].pop + '%. Plan for ' +  parsedData.forecast.simpleforecast.forecastday[location[2]].snow_allday.in + ' inches of snow.');
+			});						
+		}
+	}
 }
 
 module.exports = new messageCreator();
