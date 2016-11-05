64324e18998f50a11a10455ab20f19c85e0b2100
Seagullible <natlockhart@gmail.com>
2016-11-04 21:59:06 -0500

    Breaking out forecast message creation

--- messageCreator.js
+++ messageCreator.js
@@ -10,12 +10,12 @@ messageCreator.prototype.getWeather = function(bot,message,controller,inputLocat
 	request(url, function(error, response, data){
 		if (!error && response.statusCode == 200){
 			var parsedData = JSON.parse(data);
-			parseMessage(bot,message,controller,location,parsedData);
+			parseWeatherMessage(bot,message,controller,location,parsedData);
 		}
 	});
 }
 
-var parseMessage = function(bot,message,controller,location,parsedData){
+var parseWeatherMessage = function(bot,message,controller,location,parsedData){
 	if(parsedData.current_observation != null){
 		controller.storage.users.get(message.user, function(err, user) {        
 			bot.reply(message, location[0] + ': ' + parsedData.current_observation.temp_f +
@@ -30,16 +30,8 @@ var parseMessage = function(bot,message,controller,location,parsedData){
 	}
 }
 
-messageCreator.prototype.getForecast = function(bot,message,controller,inputLocation){
-	var request = require('request');
-	var rawLocation = inputLocation;
-	var location = rawLocation.split(',');
-	var url = 'http://api.wunderground.com/api/e6d58e1b342bc28a/forecast/q/' + location[1] + '/' + location[0] + '.json';
-	request(url, function(error, response, data){
-		if (!error && response.statusCode == 200){
-			var parsedData = JSON.parse(data);
-			
-			if(parsedData.forecast != null){
+var parseForecastMessage = function(bot,message,controller,location,parsedData){
+	if(parsedData.forecast != null){
 				if(location[2] == null){
 					controller.storage.users.get(message.user, function(err, user) {        
 					bot.reply(message, 'Today\'s ' + location[0] + ' forecast: ' + parsedData.forecast.simpleforecast.forecastday[0].conditions + '. High is ' + parsedData.forecast.simpleforecast.forecastday[0].high.fahrenheit + ', low is ' + parsedData.forecast.simpleforecast.forecastday[0].low.fahrenheit + '. ' + parsedData.forecast.simpleforecast.forecastday[0].avewind.mph + ' mph wind, ' + parsedData.forecast.simpleforecast.forecastday[0].avehumidity + '% humidity. Chance of precipitation: ' + parsedData.forecast.simpleforecast.forecastday[0].pop + '%. Plan for ' +  parsedData.forecast.simpleforecast.forecastday[0].snow_allday.in + ' inches of snow.');
@@ -51,6 +43,17 @@ messageCreator.prototype.getForecast = function(bot,message,controller,inputLoca
 					});						
 				}
 			}
+}
+
+messageCreator.prototype.getForecast = function(bot,message,controller,inputLocation){
+	var request = require('request');
+	var rawLocation = inputLocation;
+	var location = rawLocation.split(',');
+	var url = 'http://api.wunderground.com/api/e6d58e1b342bc28a/forecast/q/' + location[1] + '/' + location[0] + '.json';
+	request(url, function(error, response, data){
+		if (!error && response.statusCode == 200){
+			var parsedData = JSON.parse(data);
+			parseForecastMessage(bot,message,controller,location,parsedData);
 		}
 	});
 }
